69ec9a67b6a9cb9f9411f93e07e94d0537279d90
Gustavo Carvalho <gsc13@tuta.io>
2024-04-13 00:55:02 -0300

    Add UpdateGenre use-case

---
+++ app/genre/application/use_case/update_genre.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module Application
+  module UseCase
+    class UpdateGenreRequest < Dry::Struct
+      attribute :id, Types::UUID
+      attribute :name, Types::String
+      attribute :categories, Types::Array.of(Types::UUID)
+      attribute :is_active, Types::Bool
+    end
+
+    class UpdateGenre
+      def initialize(genre_repository:, category_repository:)
+        @genre_repository = genre_repository
+        @category_repository = category_repository
+      end
+
+      def execute(request_dto)
+        genre = @genre_repository.get_by_id(id: request_dto.id)
+        raise Exceptions::GenreNotFound.new(id: request_dto.id) if genre.nil?
+
+        genre_name = genre.name
+        genre_name = request_dto.name if request_dto.name
+
+        genre_is_active = genre.is_active
+        genre_is_active = request_dto.is_active if request_dto.is_active
+
+        request_dto.categories.each do |id|
+          category = @category_repository.get_by_id(id:)
+
+          raise Exceptions::RelatedCategoriesNotFound.new(id:) if category.nil?
+        end
+
+        genre_categories = genre.categories
+        genre_categories = request_dto.categories if request_dto.categories
+
+        updated_genre = Domain::Genre.new(
+          id: genre.id,
+          name: genre_name,
+          categories: genre_categories,
+          is_active: genre_is_active
+        )
+
+        @genre_repository.update(updated_genre)
+      rescue ArgumentError => e
+        raise(Exceptions::InvalidGenreData, e)
+      end
+    end
+  end
+end
---
+++ spec/genre/unit/application/update_genre_spec.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Application::UseCase::UpdateGenre do
+  it 'when genre does not exist should raise error' do
+    genre_repository = instance_double(Domain::GenreRepository, get_by_id: nil)
+    category_repository = instance_double(Domain::CategoryRepository, get_by_id: nil)
+
+    use_case = Application::UseCase::UpdateGenre.new(genre_repository:, category_repository:)
+    request_dto = Application::UseCase::UpdateGenreRequest.new(
+      id: SecureRandom.uuid,
+      name: 'Horror',
+      categories: [],
+      is_active: true
+    )
+
+    expect { use_case.execute(request_dto) }.to(
+      raise_error(Exceptions::GenreNotFound)
+    )
+  end
+
+  it 'when genre request has invalid params should raise invalid genre error' do
+    genre = Domain::Genre.new(name: 'Comedy')
+    genre_repository = instance_double(Domain::GenreRepository, get_by_id: genre)
+    category_repository = instance_double(Domain::CategoryRepository, get_by_id: nil)
+
+    use_case = Application::UseCase::UpdateGenre.new(genre_repository:, category_repository:)
+    request_dto = Application::UseCase::UpdateGenreRequest.new(
+      id: genre.id,
+      name: '',
+      categories: [],
+      is_active: true
+    )
+
+    expect { use_case.execute(request_dto) }.to(
+      raise_error(Exceptions::InvalidGenreData, 'name must be present')
+    )
+  end
+
+  it 'when provided category id does not exist should raise related category error' do
+    genre = Domain::Genre.new(name: 'Comedy')
+    genre_repository = instance_double(Domain::GenreRepository, get_by_id: genre)
+    category_repository = instance_double(Domain::CategoryRepository, get_by_id: nil)
+
+    unkown_id = SecureRandom.uuid
+    use_case = Application::UseCase::UpdateGenre.new(genre_repository:, category_repository:)
+    request_dto = Application::UseCase::UpdateGenreRequest.new(
+      id: genre.id,
+      name: 'Western',
+      categories: [unkown_id],
+      is_active: true
+    )
+
+    expect { use_case.execute(request_dto) }.to(
+      raise_error(Exceptions::RelatedCategoriesNotFound,
+                  "Category with id #{unkown_id} not found")
+    )
+  end
+
+  it 'should update genre from repository' do
+    category = Domain::Category.new(name: 'Movie')
+    category_repository = instance_double(Domain::CategoryRepository, get_by_id: category)
+
+    genre = Domain::Genre.new(name: 'Comedy', is_active: false)
+    genre_repository = instance_double(Domain::GenreRepository, get_by_id: genre, update: nil)
+
+    expect(genre_repository.get_by_id(id: genre.id)).to(eq(genre))
+
+    use_case = Application::UseCase::UpdateGenre.new(
+      genre_repository:,
+      category_repository:
+    )
+
+    request_dto = Application::UseCase::UpdateGenreRequest.new(
+      id: genre.id,
+      name: 'Horror',
+      categories: [category.id],
+      is_active: true
+    )
+
+    use_case.execute(request_dto)
+
+    expect(genre_repository).to(
+      have_received(:update).with(
+        Domain::Genre.new(
+          id: genre.id,
+          name: 'Horror',
+          is_active: true,
+          categories: [category.id]
+        )
+      )
+    )
+  end
+end
