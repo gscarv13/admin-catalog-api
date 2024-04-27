59dc0ada3e5100f54c4e75f4ac63854176290b50
Gustavo Carvalho <gsc13@tuta.io>
2024-04-26 23:17:46 -0300

    Move CategoryMapper to its own file

--- core/category/infra/repository/active_record_category_repository.rb
+++ core/category/infra/repository/active_record_category_repository.rb
@@ -2,30 +2,6 @@
 
 module Infra
   module Repository
-    class CategoryMapper
-      def initialize(model: nil)
-        @model = model || Category
-      end
-
-      def to_entity(model)
-        Domain::Category.new(
-          id: model.id,
-          name: model.name,
-          description: model.description,
-          is_active: model.is_active
-        )
-      end
-
-      def to_model(entity)
-        @model.new(
-          id: entity.id,
-          name: entity.name,
-          description: entity.description,
-          is_active: entity.is_active
-        )
-      end
-    end
-
     class ActiveRecordCategoryRepository < Domain::CategoryRepository
       include Pagination
       attr_reader :categories
