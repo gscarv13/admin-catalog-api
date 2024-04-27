c082cc8f990866ca91a001f25a9c367a3515348b
Gustavo Carvalho <gsc13@tuta.io>
2024-04-27 00:54:42 -0300

    Improve Pagination module

--- core/shared/pagination.rb
+++ core/shared/pagination.rb
@@ -4,10 +4,14 @@ module Pagination
   DEFAULT_PAGE_SIZE = 10
 
   def paginate(scope:, page: 1, page_size: DEFAULT_PAGE_SIZE)
+    return scope if page.nil?
+
     scope.offset((page - 1) * page_size).limit(page_size)
   end
 
   def order_by(scope:, order_by:)
+    return scope if order_by.nil?
+
     scope.order(order_by)
   end
 end
