180de582b18792c8f89b9c7d03b2c6f5a3aff856
Gustavo Carvalho <gsc13@tuta.io>
2024-04-28 15:59:56 -0300

    Remove unnecessary module mention

--- core/shared/types.rb
+++ core/shared/types.rb
@@ -5,7 +5,7 @@ require 'dry-types'
 module Types
   include Dry.Types
 
-  UUID = Types::String.constrained(
+  UUID = String.constrained(
     format: /\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z/i
   ).freeze
 end
